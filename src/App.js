a24182045bc48e526bf0d1abec4d6ddef4e24d35
cabbage <peterdelmastro@hotmail.com>
2021-04-17 20:51:42 -0400

    theres a grid!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import useKeypress from 'react-use-keypress'
+import DivObject from './DivObject';
+
+
+
+ 
+
+
+
 
 function App() {
+  const [divsArray, setDivsArray] = useState([]);
+  const [playerPosition, setPlayerPostion] = useState(0);
+
+
+  // setPlayerPostion(divsArray[1]);
+
+
+  //connect players div position to movement function]
+
+  //pick up player move
+  //check to see if move is valid (does the )
+  //if valid, move
+  //movement function to set players div position 
+
+  
+  
+//populate the array 
+const populateArray = function (howMany) {
+  const innerArray = [];
+  for (let i = 0; i < howMany; i++){
+    const item = DivObject('blue');
+    innerArray.push(item);
+  }
+  setDivsArray(innerArray);
+}
+  
+  //useEffect to fetch our data from function
+  useEffect(() => {
+
+    populateArray(100
+    );
+    console.log(divsArray[1]);
+
+  }, [])
+
+  useKeypress(['ArrowLeft', 'ArrowRight'], (event) => {
+    if (event.key === 'ArrowLeft') {
+      console.log(event.target);
+    }
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>Bitchin' Test Zone</h1>
       </header>
+      <main>
+       <div className="mazeContainer">
+        <div className="flexContainer">
+      
+            {divsArray.map((item) => {
+                return(
+                  <div class = 'square' key={item.key} tabIndex='0'>
+
+                    {item}   
+             
+                  </div>
+                )
+            })}
+       
+          </div>
+          </div>
+
+      </main>
     </div>
   );
 }
---
+++ src/DivObject.js
@@ -0,0 +1,8 @@
+
+function DivObject(props) {
+    return (
+        <div className={props.color}></div>
+    )
+}
+
+export default DivObject;
